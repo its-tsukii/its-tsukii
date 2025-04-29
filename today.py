f4e113b106b6023eb872e52d99b48d8bfedab4e5
AAYUSH KUKADE <akukade10@gmail.com>
2025-04-30 04:18:52 +0530

    Most Probably my final commit

--- today.py
+++ today.py
@@ -322,6 +322,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
+    justify_format(root, 'age_data', age_data) #calculation of the date of birth till years in months days year
     justify_format(root, 'commit_data', commit_data, 22)
     justify_format(root, 'star_data', star_data, 14)
     justify_format(root, 'repo_data', repo_data, 6)
